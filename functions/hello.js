02ad4e910ae846e63c9c999f074715ce896ae439
jebin2 <jebineinstein@gmail.com>
2024-08-08 14:26:16 +0530

    moved await to handler.

--- functions/hello.js
+++ functions/hello.js
@@ -6,18 +6,20 @@ const {
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
 
-let { data: id, create_at, base_currency, generate_time } = await supabase
-    .from('currency')
-    .select('*')
 exports.handler = async function(event, context) {
-    
-    // const { data, error } = await supabase
-    //     .from('notes')
-    //     .insert([
-    //         { note: 'I need to not forget this' },
-    //     ]);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: data })
-  };
+    let { data, error } = await supabase
+        .from('currency')
+        .select('*');
+
+    if (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ data }),
+    };
 };
