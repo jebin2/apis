9d6a381e812b102c51c3c5fe417e93829d531efd
jebin2 <jebineinstein@gmail.com>
2024-08-08 14:19:17 +0530

    added supabase code.

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,23 @@
+const {
+    DATABASE_URL,
+    SUPABASE_SERVICE_API_KEY
+} = process.env;
+
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+
+let { data: id, create_at, base_currency, generate_time } = await supabase
+    .from('currency')
+    .select('*')
 exports.handler = async function(event, context) {
+    
+    // const { data, error } = await supabase
+    //     .from('notes')
+    //     .insert([
+    //         { note: 'I need to not forget this' },
+    //     ]);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, World!" })
+    body: JSON.stringify({ message: data })
   };
 };
