626db0bef3d32e960453cc9ba7af486a40e9f892
jebin2 <jebineinstein@gmail.com>
2024-08-17 17:01:59 +0530

    google auth api.

---
+++ functions/googleAuth.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const {
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URI
+} = process.env;
+
+const { google } = require('googleapis');
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+    const code = event.queryStringParameters.code;
+    const clientId = GOOGLE_CLIENT_ID;
+    const clientSecret = GOOGLE_CLIENT_SECRET;
+    const redirectUri = GOOGLE_REDIRECT_URI;
+
+    const oauth2Client = new google.auth.OAuth2(
+        clientId,
+        clientSecret,
+        redirectUri
+    );
+
+    try {
+        const { tokens } = await oauth2Client.getToken(code);
+        oauth2Client.setCredentials(tokens);
+
+        const drive = google.drive({ version: 'v3', auth: oauth2Client });
+        const res = await drive.files.list();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(res.data),
+        };
+    } catch (error) {
+        console.error('Error getting tokens:', error);
+        return {
+            statusCode: 500,
+            body: 'Error getting tokens',
+        };
+    }
+};
